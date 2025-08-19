8fb8c09219c4795a05d21f3d66c7c7dbe968fed9
Jayyy25 <184485982+Jayyyyy25@users.noreply.github.com>
2025-08-19 12:49:55 +0800

    Rename and Greet

---
+++ src/main/java/Remy.java
@@ -0,0 +1,19 @@
+public class Remy {
+//    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+//    }
+    public static void main(String[] args) {
+        String name = "Remy";
+        String divider = "-----------------------------";
+        System.out.println(divider);
+        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. Hope to see you soon!");
+        System.out.println(divider);
+    }
+}
