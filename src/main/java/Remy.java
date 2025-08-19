5ab7f9f072523c9055ec1eb3c924943515b1bc9d
Jayyy25 <184485982+Jayyyyy25@users.noreply.github.com>
2025-08-19 14:15:25 +0800

    Add support for echo

--- src/main/java/Remy.java
+++ src/main/java/Remy.java
@@ -1,19 +1,55 @@
+import java.util.Scanner;
+
 public class Remy {
-//    public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-//    }
+    private static String name = "Remy";
+    private static String divider = "----------------------------------------";
+    private static String logo =
+            " ____  _____ __  __ __     __\n" +
+            "|  _ \\| ____|  \\/  |\\ \\   / /\n" +
+            "| |_) |  _| | |\\/| | \\ \\ / / \n" +
+            "|  _ <| |___| |  | |  \\ V /  \n" +
+            "|_| \\_\\_____|_|  |_|   |_|   \n";
+    private static boolean running = true;
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String name = "Remy";
-        String divider = "-----------------------------";
-        System.out.println(divider);
-        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?");
-        System.out.println(divider);
-        System.out.println("Bye. Hope to see you soon!");
-        System.out.println(divider);
+        greetUser();
+
+        while (running) {
+            System.out.println(divider);
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                running = false;
+                System.out.println(divider);
+                exitBox();
+            } else {
+                System.out.println(divider);
+                respond(userInput);
+            }
+        }
+    }
+
+    private static void greetUser() {
+        System.out.println(logo);
+        System.out.println("\t\t\tHello! I'm " + name + ".\n" + "\t\t\tWhat can I do for you?");
+    }
+
+    private static void exitBox() {
+        System.out.println("\t\t\tBye. Hope to see you soon!");
+    }
+
+    private static void respond(String input) {
+        switch (input) {
+            case "list":
+                listing();
+                break;
+            default:
+                System.out.println("\t\t\t" + input);
+                break;
+        }
+    }
+
+    private static void listing() {
+        System.out.println("\t\t\tlist");
     }
 }
