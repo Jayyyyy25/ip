e8a3489940a642e039f04fe5a9eb7d2dc6e55158
Jayyy25 <184485982+Jayyyyy25@users.noreply.github.com>
2025-08-19 15:43:19 +0800

    Add support for mark & unmark done

--- src/main/java/Remy.java
+++ src/main/java/Remy.java
@@ -12,7 +12,7 @@ public class Remy {
             "|_| \\_\\_____|_|  |_|   |_|   \n";
     private static boolean running = true;
     private static Scanner scanner = new Scanner(System.in);
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         greetUser();
@@ -21,7 +21,12 @@ public class Remy {
             System.out.println(divider);
             String userInput = scanner.nextLine();
             System.out.println(divider);
-            respond(userInput);
+
+            // Split the inputs into command and arguments for easier case handling
+            String[] parts = userInput.split(" ", 2);
+            String command = parts[0];
+            String argument = parts.length > 1 ? parts[1] : "";
+            respond(command, argument);
         }
     }
 
@@ -30,8 +35,8 @@ public class Remy {
         System.out.println("\t\t\tHello! I'm " + name + ".\n" + "\t\t\tWhat can I do for you?");
     }
 
-    private static void respond(String input) {
-        switch (input) {
+    private static void respond(String command, String argument) {
+        switch (command) {
             case "bye":
                 running = false;
                 exitBot();
@@ -39,8 +44,14 @@ public class Remy {
             case "list":
                 listing();
                 break;
+            case "mark":
+                markAsDone(Integer.parseInt(argument) - 1);
+                break;
+            case "unmark":
+                markAsUndone(Integer.parseInt(argument) - 1);
+                break;
             default:
-                add(input);
+                add(command + argument);
                 break;
         }
     }
@@ -50,14 +61,33 @@ public class Remy {
     }
 
     private static void listing() {
+        // Headings for list
+        if (tasks.isEmpty()) {
+            System.out.println("\t\t\tYou have no tasks in the list at this moment.");
+        } else {
+            System.out.println("\t\t\tHere are the tasks in the list.");
+        }
+
         for (int i = 0; i < tasks.size(); i++) {
             int ind = i + 1;
-            System.out.println("\t\t\t" + ind + ": " + tasks.get(i));
+            System.out.println("\t\t\t" + ind + "." + tasks.get(i).getStatus());
         }
     }
 
-    private static void add(String task) {
-        tasks.add(task);
-        System.out.println("\t\t\tadded: " + task);
+    private static void add(String title) {
+        tasks.add(new Task(title));
+        System.out.println("\t\t\tadded: " + title);
+    }
+
+    private static void markAsDone(int taskInd) {
+        tasks.get(taskInd).markAsDone();
+        System.out.println("\t\t\tNice! I've marked this task as done:");
+        System.out.println("\t\t\t" + tasks.get(taskInd).getStatus());
+    }
+
+    private static void markAsUndone(int taskInd) {
+        tasks.get(taskInd).markAsUndone();
+        System.out.println("\t\t\tOk, I've marked this task as not done yet:");
+        System.out.println("\t\t\t" + tasks.get(taskInd).getStatus());
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    private String title;
+    private boolean isDone;
+
+    public Task(String title) {
+        this.title = title;
+        this.isDone = false;
+    }
+
+    public String getStatus() {
+        return (isDone ? "[X] " : "[ ] ") + this.title;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+}
