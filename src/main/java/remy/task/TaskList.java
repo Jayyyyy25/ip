c5bc435673ab23e6dc0f15663e34b4b5b23b1076
Jayyy25 <184485982+Jayyyyy25@users.noreply.github.com>
2025-09-11 12:01:46 +0800

    Fixes checkstyle error

--- src/main/java/remy/task/TaskList.java
+++ src/main/java/remy/task/TaskList.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.*;
+import java.util.stream.Collectors;
 
 import remy.exception.InvalidArgumentException;
 import remy.exception.RemyException;
