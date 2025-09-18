d9450315a306a7b3a1c36b5353db70eff9d94ebb
Jayyy25 <184485982+Jayyyyy25@users.noreply.github.com>
2025-09-18 20:59:35 +0800

    Fixes unused imports

--- src/main/java/remy/ui/Main.java
+++ src/main/java/remy/ui/Main.java
@@ -7,7 +7,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import remy.exception.RemyException;
 
 /**
  * Main class for running the GUI of Remy application
